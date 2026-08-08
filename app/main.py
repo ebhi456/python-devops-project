9e8202d83345d9c6cd07561d48cabe9c3f0006eb
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-08 21:08:07 +0530

    add employee api's

--- app/main.py
+++ app/main.py
@@ -1,10 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
 
 app = FastAPI(
     title="Employee Management API",
     description="DevOps demonstration project",
     version="1.0.0"
 )
+
+
+class Employee(BaseModel):
+    name: str
+    email: str
+    department: str
+    salary: float
+
+
+employees = {
+    1: {
+        "id": 1,
+        "name": "Rahul",
+        "email": "rahul@example.com",
+        "department": "DevOps",
+        "salary": 75000
+    },
+    2: {
+        "id": 2,
+        "name": "Priya",
+        "email": "priya@example.com",
+        "department": "Cloud",
+        "salary": 85000
+    }
+}
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome to Employee Management API"
+    }
 
 
 @app.get("/health")
@@ -15,8 +49,72 @@ def health_check():
     }
 
 
-@app.get("/")
-def root():
+@app.get("/employees")
+def get_employees():
+    return list(employees.values())
+
+
+@app.get("/employees/{employee_id}")
+def get_employee(employee_id: int):
+
+    if employee_id not in employees:
+        raise HTTPException(
+            status_code=404,
+            detail="Employee not found"
+        )
+
+    return employees[employee_id]
+
+
+@app.post("/employees")
+def create_employee(employee: Employee):
+
+    employee_id = max(employees.keys()) + 1
+
+    new_employee = {
+        "id": employee_id,
+        **employee.model_dump()
+    }
+
+    employees[employee_id] = new_employee
+
+    return new_employee
+
+
+@app.put("/employees/{employee_id}")
+def update_employee(
+    employee_id: int,
+    employee: Employee
+):
+
+    if employee_id not in employees:
+        raise HTTPException(
+            status_code=404,
+            detail="Employee not found"
+        )
+
+    updated_employee = {
+        "id": employee_id,
+        **employee.model_dump()
+    }
+
+    employees[employee_id] = updated_employee
+
+    return updated_employee
+
+
+@app.delete("/employees/{employee_id}")
+def delete_employee(employee_id: int):
+
+    if employee_id not in employees:
+        raise HTTPException(
+            status_code=404,
+            detail="Employee not found"
+        )
+
+    deleted_employee = employees.pop(employee_id)
+
     return {
-        "message": "Welcome to Employee Management API"
+        "message": "Employee deleted successfully",
+        "employee": deleted_employee
     }
