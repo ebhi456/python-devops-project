9efed0e05418bc423ba1c5178099fed86d496ed1
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-10 04:44:44 +0530

    ecr related config for ec2 removed

--- terraform/iam_ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_role_policy_attachment" "employee_api_ecr_readonly_policy_attachment" {
-  role       = aws_iam_role.employee_api_ec2_role.name
-  policy_arn = aws_iam_policy.employee_api_ec2_role_policy.arn
-}
