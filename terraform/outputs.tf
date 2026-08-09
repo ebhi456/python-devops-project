a1b9c15932a9fac4154a4c986657934075f4c5e9
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-10 02:03:01 +0530

    created the new ec2 instance for the application

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,23 @@ output "ec2_role_name" {
   description = "Name of the IAM role for EC2"
   value       = aws_iam_role.employee_api_ec2_role.name
 }
+
+output "ec2_instance_profile_name" {
+  description = "Name of the IAM instance profile for EC2"
+  value       = aws_iam_instance_profile.employee_api_ec2_instance_profile.name
+}
+
+output "vpc_id" {
+  description = "employee api vpc id"
+  value = aws_vpc.employee_api_vpc.id
+}
+
+output "public_subnet_id" {
+  description = "employee api public subnet id"
+  value = aws_subnet.public.id
+}
+
+output "security_group_id" {
+  description = "employee api security group id"
+  value = aws_security_group.employee_api_sg.id
+}
