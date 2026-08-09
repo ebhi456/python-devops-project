432cd411111a89eceac7fa2f959dbaa4f4b7e934
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-10 04:55:24 +0530

    vpc changes for eks

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,18 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.employee_api_vpc.id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
 output "ecr_repository_name" {
   description = "ECR repository name"
   value       = aws_ecr_repository.employee_api.name
@@ -7,20 +22,3 @@ output "ecr_repository_url" {
   description = "ECR repository URL"
   value       = aws_ecr_repository.employee_api.repository_url
 }
-
-
-output "vpc_id" {
-  description = "employee api vpc id"
-  value = aws_vpc.employee_api_vpc.id
-}
-
-output "public_subnet_id" {
-  description = "employee api public subnet id"
-  value = aws_subnet.public.id
-}
-
-output "security_group_id" {
-  description = "employee api security group id"
-  value = aws_security_group.employee_api_sg.id
-}
-
