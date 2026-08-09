0934262c26842a056b3ed1e11e874d32813b03ac
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-10 01:46:52 +0530

    route table association is added

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -52,4 +52,9 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 
