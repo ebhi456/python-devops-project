e04511f4379e314e296330b315c33b01885faea9
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-10 00:28:32 +0530

    changes in iam file due to resource reference issue

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -23,3 +23,33 @@ resource "aws_iam_role" "employee_api_ec2_role" {
     ManagedBy   = "Terraform"
   }
 }
+
+resource "aws_iam_policy" "employee_api_ec2_role_policy" {
+  name        = "${var.project_name}-ec2-role-policy"
+  description = "IAM policy for EC2 role to access ECR repository"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "${var.project_name}-ec2-role-policy"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
