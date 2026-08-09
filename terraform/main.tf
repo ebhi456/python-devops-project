5f25ae58ce8068f40ad12446bde22e2af6bf861d
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-10 01:25:18 +0530

    module issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,12 @@ resource "aws_ecr_repository" "employee_api" {
   }
   
 }
+
+
+module "networking" {
+  source = "./networking"
+
+  project_name             = var.project_name
+  vpc_cidr_block           = var.vpc_cidr_block
+  public_subnet_cidr_block = var.public_subnet_cidr_block
+}
