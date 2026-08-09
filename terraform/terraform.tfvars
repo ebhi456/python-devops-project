1cf719b7d6b84761c0dfd41b735fef29ff724649
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-10 04:58:50 +0530

    variable changes

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,18 @@
+project_name   = "python-devops-project"
+aws_region     = "us-east-1"
 vpc_cidr_block = "10.0.0.0/16"
-public_subnet_cidr_block = "10.0.1.0/24"
-project_name = "python-devops-project"
+
+availability_zones = [
+  "us-east-1a",
+  "us-east-1b"
+]
+
+public_subnet_cidrs = [
+  "10.0.1.0/24",
+  "10.0.2.0/24"
+]
+
+private_subnet_cidrs = [
+  "10.0.11.0/24",
+  "10.0.12.0/24"
+]
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,24 +1,45 @@
 variable "aws_region" {
-  description = "aws region definition"
-  type = string
-  default = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "project_name" {
-  description = "project name definition"
-  type = string
-  #default = "python-devops-project"
+  description = "Project name"
+  type        = string
 }
 
 variable "vpc_cidr_block" {
-  description = "VPC CIDR block definition"
-  type = string
-  #default = "10.0.0.0/16"
+  description = "VPC CIDR block"
+  type        = string
 }
 
-variable "public_subnet_cidr_block" {
-  description = "Public Subnet CIDR block definition"
-  type = string
-  #default = "10.0.1.0/24"
+variable "availability_zones" {
+  description = "Availability zones for the EKS VPC"
+  type        = list(string)
+
+  default = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
 }
 
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+
+  default = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+}
