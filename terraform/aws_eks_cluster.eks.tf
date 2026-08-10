5111483d74f32fdb0eb39f07ea1183febf2a12fe
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-10 07:34:16 +0530

    addon issue

--- terraform/aws_eks_cluster.eks.tf
+++ terraform/aws_eks_cluster.eks.tf
@@ -1,9 +1,9 @@
 resource "aws_eks_addon" "ebs_csi" {
-  cluster_name = aws_eks_cluster.eks.name
+  cluster_name = aws_eks_cluster.employee_api.name
 
-  addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.63.1-eksbuild.1"
-  service_account_role_arn    = aws_iam_role.ebs_csi.arn
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi.arn
+
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 
--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -53,17 +53,3 @@ resource "aws_iam_role_policy_attachment" "ebs_csi" {
   role       = aws_iam_role.ebs_csi.name
   policy_arn = data.aws_iam_policy.ebs_csi.arn
 }
-
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name = aws_eks_cluster.employee_api.name
-
-  addon_name               = "aws-ebs-csi-driver"
-  service_account_role_arn = aws_iam_role.ebs_csi.arn
-
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [
-    aws_iam_role_policy_attachment.ebs_csi
-  ]
-}
