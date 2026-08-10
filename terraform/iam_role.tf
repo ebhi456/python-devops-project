af6d08a5a1211ba98d9eed1b0722d06eb3a421b7
Ebinejar Nagubamu <nagubamu.ebinejar@tanla.com>
2026-08-10 07:31:29 +0530

    role issue

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -3,12 +3,12 @@ data "aws_iam_policy" "ebs_csi" {
 }
 
 data "aws_iam_openid_connect_provider" "eks" {
-  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+  url = aws_eks_cluster.employee_api.identity[0].oidc[0].issuer
 }
 
 locals {
   oidc_provider = replace(
-    aws_eks_cluster.eks.identity[0].oidc[0].issuer,
+    aws_eks_cluster.employee_api.identity[0].oidc[0].issuer,
     "https://",
     ""
   )
@@ -36,7 +36,7 @@ data "aws_iam_policy_document" "ebs_csi_assume_role" {
     condition {
       test     = "StringEquals"
       variable = "${local.oidc_provider}:sub"
-      values   = [
+      values = [
         "system:serviceaccount:kube-system:ebs-csi-controller-sa"
       ]
     }
@@ -52,4 +52,18 @@ resource "aws_iam_role" "ebs_csi" {
 resource "aws_iam_role_policy_attachment" "ebs_csi" {
   role       = aws_iam_role.ebs_csi.name
   policy_arn = data.aws_iam_policy.ebs_csi.arn
+}
+
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = aws_eks_cluster.employee_api.name
+
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi.arn
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.ebs_csi
+  ]
 }
